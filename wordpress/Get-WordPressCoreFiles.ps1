bf0cc028bd2ccf0bcf59633626768dad822a1d31
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-04-24 01:05:04 +0200

    Deleting .gitkeep file after downloading WordPress core files

--- wordpress/Get-WordPressCoreFiles.ps1
+++ wordpress/Get-WordPressCoreFiles.ps1
@@ -33,5 +33,6 @@ $_wordpress_path = $RootPath + $Constants.paths.wordpress
 $_version = Get-WpCliCoreDownloadVersion $SiteConfigJson.settings.version $Constants.defaults.version
 $_locale = Get-WpCliCoreDownloadLocale $SiteConfigJson.settings.locale $Constants.defaults.locale
 
-# Download WordPress without the default themes and plugins
+# Download WordPress without the default themes and plugins and delete .gitkeep file
 wp core download --version=$_version --locale=$_locale --path=$_wordpress_path --skip-content
+if (Test-Path "$_wordpress_path/.gitkeep") { Remove-Item "$_wordpress_path/.gitkeep" }
