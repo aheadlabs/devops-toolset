d6c90beebf5851cc55880d2277db305353e5013b
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-04-28 21:33:44 +0200

    Using parameters for paths in the Bash script

--- wordpress/Install-WpCli.sh
+++ wordpress/Install-WpCli.sh
@@ -1,9 +1,9 @@
 # Download PHAR file
-curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o wp-cli.phar
+curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o "$1/wp-cli.phar"
 
 # Grant it execution permission and move it to user's execution path
-chmod +x wp-cli.phar
-sudo mv wp-cli.phar /usr/local/bin/wp
+chmod +x "$1/wp-cli.phar"
+sudo mv "$1/wp-cli.phar" /usr/local/bin/wp
 
 # Show WP CLI information
 wp --info
