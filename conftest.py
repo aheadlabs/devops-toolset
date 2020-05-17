32e138d63562ca2dc842c409bc12b00883ca51c1
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-18 00:04:01 +0200

    Moved fixtures to conftest.py at package level.

---
+++ conftest.py
@@ -0,0 +1,25 @@
+"""Test configuration file for devops-toolset package.
+
+This code is executed once per unit test session.
+Add here whatever you want to pass as a fixture in your texts.
+
+    ie: (see FileNames example)
+        - Add a class that contains what you want to pass as a fixture in your tests.
+        - Create a fixture with that same lowered name that returns an instance to that class."""
+
+import pytest
+
+class FileNames(object):
+    """Class used to create the filenames fixture"""
+    test_file = "test.py"
+    path = "/pathto"
+    deep_path = "/deep/pathto"
+    paths = ["/pathto/file1", "/pathto/file2"]
+    no_paths = []
+    file = ".gitignore"
+    file__path = "filesystem.paths.__file__"
+
+@pytest.fixture
+def filenames():
+    """Sample file names for testing file system related functionality"""
+    return FileNames()
--- filesystem/tests/conftest.py
+++ filesystem/tests/conftest.py
@@ -1,24 +1,3 @@
 """Test configuration file for filesystem module.
 
-Add here whatever you want to pass as a fixture in your texts.
-
-    ie: (see FileNames example)
-        - Add a class that contains what you want to pass as a fixture in your tests.
-        - Create a fixture with that same lowered name that returns an instance to that class."""
-
-import pytest
-
-class FileNames(object):
-    """Class used to create the filenames fixture"""
-    test_file = "test.py"
-    path = "/pathto"
-    deep_path = "/deep/pathto"
-    paths = ["/pathto/file1", "/pathto/file2"]
-    no_paths = []
-    file = ".gitignore"
-    file__path = "filesystem.paths.__file__"
-
-@pytest.fixture
-def filenames():
-    """Sample file names for testing file system related functionality"""
-    return FileNames()
+Add here whatever you want to pass as a fixture in your texts."""
