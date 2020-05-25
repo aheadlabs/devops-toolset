2726bdfc45e578e6510f109e633e1644cf35fd35
Alberto Carbonell Rubio <cr.alberto2@gmail.com>
2020-05-25 18:27:56 +0200

    Fixed log_setup tests typos and smells

--- core/tests/conftest.py
+++ core/tests/conftest.py
@@ -1,21 +1,27 @@
 """Test configuration file for core module.
 
     contains:
-        - Class CoreTestsConstants: contains some static values used in tests"""
+        - Class CoreTestsFixture: contains some static values used in tests"""
 
 import pytest
 import logging
+import pathlib
+import os
+
 
 class CoreTestsFixture(object):
     """Class used to create the test constants fixture"""
     default_loglevel = logging.INFO
     default_handler = logging.StreamHandler()
-    fake_config_file_path = "./core/tests/logging_config.json"
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    root_path = pathlib.Path(current_path).absolute()
+    fake_config_file_path = pathlib.Path.joinpath(root_path, 'logging_config.json').__str__()
     fake_error_exception_message = "Exception error message"
-    couldnt_config_file_message = f"Couldn't configure logger: {fake_error_exception_message}"
-    default_configure_success = "Default configuration loaded succesfully."
+    cannot_config_message = f"Cannot configure logger: {fake_error_exception_message}"
+    default_configure_success = "Default configuration loaded successfully."
     fake_config_data_file_content = r'{"logging": {"foo": "foo"}}'
     file_open_patch = "builtins.open"
+
 
 @pytest.fixture
 def coretestsfixture():
--- core/tests/log_setup_test.py
+++ core/tests/log_setup_test.py
@@ -1,118 +1,126 @@
 """Unit tests for the log_setup file"""
 
 import logging
-import os
 from unittest.mock import patch, mock_open
 import core.log_setup as sut
-import json
-from core.tests.conftest import CoreTestsFixture as fixture
+from core.tests.conftest import CoreTestsFixture as Fixture
 
-#region configure(filepath)
+# region configure(filepath)
 
-def test_configure_given_filepath_when_exception_catched_then_logs_error():
-    """Given a filepath to configure logging, when exception is catched, then logs
+
+def test_configure_given_filepath_when_exception_catch_then_logs_error():
+    """Given a filepath to configure logging, when exception is catch, then logs
     with error severity"""
 
     # Arrange
-    filepath = fixture.fake_config_file_path
-    expected_message = fixture.couldnt_config_file_message
-    with patch.object(sut, "configure_by_file" ) as configure_by_file_mock:
-        configure_by_file_mock.side_effect = Exception(fixture.fake_error_exception_message)
+    filepath = Fixture.fake_config_file_path
+    expected_message = Fixture.cannot_config_message
+    with patch.object(sut, "configure_by_file") as configure_by_file_mock:
+        configure_by_file_mock.side_effect = Exception(Fixture.fake_error_exception_message)
         with patch.object(logging, "error") as logger_error_mock:
             # Act
             sut.configure(filepath)
             # Assert
             logger_error_mock.assert_called_once_with(expected_message)
 
-def test_configure_given_filepath_when_exception_catched_then_configure_by_default():
-    """Given a filepath to configure logging, when exception is catched, then calls
+
+def test_configure_given_filepath_when_exception_catch_then_configure_by_default():
+    """Given a filepath to configure logging, when exception is catch, then calls
     configure logger by default"""
 
     # Arrange
-    filepath = fixture.fake_config_file_path
-    with patch.object(sut, "configure_by_file" ) as configure_by_file_mock:
-        configure_by_file_mock.side_effect = Exception(fixture.fake_error_exception_message)
+    filepath = Fixture.fake_config_file_path
+    with patch.object(sut, "configure_by_file") as configure_by_file_mock:
+        configure_by_file_mock.side_effect = Exception(Fixture.fake_error_exception_message)
         with patch.object(sut, "configure_by_default") as configure_by_default_mock:
             # Act
             sut.configure(filepath)
             # Assert
             configure_by_default_mock.assert_called_once()
 
+
 def test_configure_given_filepath_then_configure_by_file():
     """Given a filepath to configure logging, then calls
     configure logger by file"""
 
     # Arrange
-    filepath = fixture.fake_config_file_path
+    filepath = Fixture.fake_config_file_path
     with patch.object(sut, "configure_by_file") as configure_by_file_mock:
         # Act
         sut.configure(filepath)
         # Assert
         configure_by_file_mock.assert_called_once()
 
-#endregion
+# endregion
 
-#region configure_by_default(loglevel)
+# region configure_by_default(loglevel)
+
 
 @patch.object(logging, "getLogger")
 def test_configure_by_default_sets_root_logger_level(get_logger_mock):
     """Given a log level, should set loglevel to the root logger."""
-    #Arrange
-    loglevel = fixture.default_loglevel
+    # Arrange
+    loglevel = Fixture.default_loglevel
     get_logger_mock.return_value = logging.getLogger(__name__)
     with patch.object(get_logger_mock.return_value, "setLevel") as logging_setlevel_mock:
-        #Act
+        # Act
         sut.configure_by_default(loglevel)
-        #Assert
+        # Assert
         logging_setlevel_mock.assert_called_once_with(loglevel)
+
 
 @patch.object(logging, "getLogger")
 def test_configure_by_default_adds_streamhandler(get_logger_mock):
     """Given a log level, should set the logging.StreamHandler() to the root logger."""
-    #Arrange
-    loglevel = fixture.default_loglevel
+    # Arrange
+    loglevel = Fixture.default_loglevel
     get_logger_mock.return_value = logging.getLogger(__name__)
     with patch.object(get_logger_mock.return_value, "addHandler") as logging_addHandler_mock:
-        #Act
+        # Act
         sut.configure_by_default(loglevel)
-        #Assert
+        # Assert
         logging_addHandler_mock.assert_called_once()
+
 
 @patch.object(logging, "getLogger")
 def test_configure_by_default_logs_info(get_logger_mock):
     """Given a log level, should log a message when configuring by default."""
-    #Arrange
-    loglevel = fixture.default_loglevel
-    expected_message = fixture.default_configure_success
+    # Arrange
+    loglevel = Fixture.default_loglevel
+    expected_message = Fixture.default_configure_success
     get_logger_mock.return_value = logging.getLogger(__name__)
     with patch.object(get_logger_mock.return_value, "info") as logging_info_mock:
-        #Act
+        # Act
         sut.configure_by_default(loglevel)
-        #Assert
+        # Assert
         logging_info_mock.assert_called_once_with(expected_message)
-#endregion
+# endregion
 
-#region configure_by_file(filepath)
+# region configure_by_file(filepath)
 
-@patch("builtins.open", new_callable=mock_open, read_data=fixture.fake_config_data_file_content)
+
+@patch("builtins.open", new_callable=mock_open, read_data=Fixture.fake_config_data_file_content)
 def test_configure_by_file_opens_filepath_in_read_mode(open_file_mock):
     """Given a filepath, should call open with read privileges."""
     # Arrange
-    filepath = fixture.fake_config_file_path
-    with patch.object(logging.config, "dictConfig") as dict_config_mock:
+    from logging import config
+    filepath = Fixture.fake_config_file_path
+    with patch.object(config, "dictConfig") as dict_config_mock:
         dict_config_mock.return_value = None
-        #Act
+        # Act
         sut.configure_by_file(filepath)
-        #Assert
+        # Assert
         open_file_mock.assert_called_once_with(filepath, "r")
+
 
 def test_configure_by_file_calls_dict_config():
     """Given a filepath, should load the config content using json module ."""
+    from logging import config
     # Arrange
-    filepath = fixture.fake_config_file_path
-    with patch.object(logging.config, "dictConfig") as dict_config_mock:
-        #Act
+    filepath = Fixture.fake_config_file_path
+    with patch.object(config, "dictConfig") as dict_config_mock:
+        # Act
         sut.configure_by_file(filepath)
-        #Assert
+        # Assert
         dict_config_mock.assert_called_once()
-#endregion
+# endregion
