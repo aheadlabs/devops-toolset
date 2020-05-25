1048b68b4f70ae05111e1250885c986c725c1034
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-25 02:23:57 +0200

    Created setup.py and requirements file.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='devops-toolset',
+    version='0.29.0',
+    packages=['core', 'core.tests', 'i18n', 'i18n.tests', 'tools', 'tools.tests', 'devops', 'wordpress', 'filesystem',
+              'filesystem.tests'],
+    url='https://github.com/aheadlabs/devops-toolset/',
+    license='https://github.com/aheadlabs/devops-toolset/blob/master/LICENSE',
+    author='Iván Sainz | Alberto Carbonell',
+    author_email='',
+    description='General purpose DevOps-related scripts and tools.',
+)
