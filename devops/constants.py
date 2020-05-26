05cf3598c583b62b2d3b00e4dc4ecccebe96f3be
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-26 17:44:28 +0200

    Added sonarx.py module

---
+++ devops/constants.py
@@ -0,0 +1,5 @@
+class Urls(object):
+    """URL constants"""
+    GITHUB_RAW_CONTENT = "https://raw.githubusercontent.com/aheadlabs/devops-toolset/master/"
+    DEVOPS_TOOLSET_MASTER = GITHUB_RAW_CONTENT + "wordpress/default-project.xml"
+    SONAR_QUALITY_GATE_PARTIAL_URL = "/api/qualitygates/project_status?projectKey="
---
+++ devops/sonarx.py
@@ -0,0 +1,97 @@
+"""sonarcloud.io / SonarQube tools"""
+
+from core.app import App
+import sys
+import requests
+import configparser
+from tools.xcoding64 import encode
+from devops.constants import Urls
+
+app: App = App()
+
+
+def get_quality_gate_status(properties_file_path: str, token: str, branch: str = None, pull_request: bool = False):
+    """Gets the status of the quality gate of a project
+
+    Args:
+        properties_file_path: Path to sonar-project.properties file. Keys
+            sonar.projectKey and sonar.organization are mandatory.
+        token: SonarCloud token used for authentication
+        branch: Git branch name to get the quality gate for. If None, master
+            will be assumed.
+        pull_request: True if the analysis was originated by a pull request.
+    """
+
+    token_base64 = encode(f"{token}:")
+    basic_auth_token = f"Basic {token_base64}"
+    headers = {"Authorization": basic_auth_token}
+
+    branch_segment = generate_branch_segment(branch, pull_request)
+
+    sonar_url, sonar_project_key, sonar_organization = read_sonar_properties_file(properties_file_path)
+
+    url = f"{sonar_url}{Urls.SONAR_QUALITY_GATE_PARTIAL_URL}{sonar_project_key}{branch_segment}"
+
+    response = requests.get(url, headers=headers)
+    quality_gate_data = response.json()
+
+    if quality_gate_data["projectStatus"]["status"] == "OK":
+        print(_("Quality gate succeeded"))
+    else:
+        for condition in quality_gate_data["projectStatus"]["conditions"]:
+            if condition["status"] == "ERROR":
+                # sys.stderr.write(_(f"Invalid metric value for {condition['metricKey']}: {condition['actualValue']}"))
+                error = _("Invalid metric value for {metricKey}: {actualValue} {comparator} {errorThreshold}")
+                sys.stderr.write(str(error + "\n").format(
+                    metricKey=condition["metricKey"],
+                    actualValue=condition["actualValue"],
+                    comparator=condition["comparator"],
+                    errorThreshold=condition["errorThreshold"]))
+
+
+def read_sonar_properties_file(path: str):
+    """Reads a sonar-project.properties file
+
+    It adds a temporary dummy section to be correctly parsed by configParser
+
+    Args:
+        path: Path to the sonar-project.properties file
+
+    Returns:
+        Tuple containing url, projectKey, organization
+    """
+
+    with open(path, "r") as file:
+        data = f"[temp]\n{file.read()}"
+
+    properties_file_parser = configparser.ConfigParser()
+    properties_file_parser.read_string(data)
+
+    sonar_url = properties_file_parser.get("temp", "sonar.host.url")
+    sonar_project_key = properties_file_parser.get("temp", "sonar.projectKey")
+    sonar_organization = properties_file_parser.get("temp", "sonar.organization")
+
+    return sonar_url, sonar_project_key, sonar_organization
+
+
+def generate_branch_segment(branch: str = None, pull_request: bool = False):
+    """Generates the branch segment or the URL used to request the quality gate
+    status
+
+    Args:
+        branch: Git branch. If None, master will be assumed.
+        pull_request: True if the analysis was originated by a pull request
+    """
+
+    if branch is None:
+        branch = "master"
+
+    if pull_request:
+        branch = branch.replace("pull/", "").replace("/merge", "")
+        return f"&pullRequest={branch}"
+    else:
+        return f"&branch={branch}"
+
+
+if __name__ == "__main__":
+    help(__name__)
