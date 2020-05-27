eeac9798d07bb0aa1843d408b4b5cef211dc1b9e
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-28 00:03:04 +0200

    setup.py now gets packages programatically.

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 """Project setup"""
 
-from setuptools import setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='devops-toolset',
     version='0.29.0',
-    packages=['core', 'core.tests', 'i18n', 'i18n.tests', 'tools', 'tools.tests', 'devops', 'wordpress', 'filesystem',
-              'filesystem.tests'],
+    packages=setuptools.find_packages(),
     url='https://github.com/aheadlabs/devops-toolset/',
     license='https://github.com/aheadlabs/devops-toolset/blob/master/LICENSE',
     author='Iván Sainz | Alberto Carbonell',
