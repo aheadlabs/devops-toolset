a9e78047bbea7d1d7ded028f40bb36c68e5da2a1
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-28 00:02:13 +0200

    Getting Getting project data from python scripts.

--- filesystem/paths.py
+++ filesystem/paths.py
@@ -3,8 +3,13 @@
 #! python
 
 import pathlib
+import xml.etree.ElementTree as ElementTree
 from typing import List
+from core.app import App
 from filesystem.constants import FileNames, Directions
+
+app: App = App()
+platform_specific = app.load_platform_specific("environment")
 
 
 def get_filepath_in_tree(file: str, direction: Directions = Directions.ASCENDING) -> str:
@@ -66,5 +71,29 @@ def get_project_root() -> str:
     return get_filepath_in_tree(FileNames.PROJECT_FILE)
 
 
+def get_project_xml_data(add_environment_variables: bool = True) -> dict:
+    """Reads the /project.xml file and returns a dict with its data.
+
+    XML elements are uppercased, underscored and prepended with parent name.
+
+    Args:
+        add_environment_variables: If True it adds every element of the dict as
+            an environment variable.
+    """
+
+    project_xml_path = pathlib.Path.joinpath(app.settings.root_path, "project.xml")
+    xml = ElementTree.parse(str(project_xml_path)).getroot()
+
+    environment_variables = {}
+    for e in xml:
+        environment_variables[f"{xml.tag}_{e.tag}".upper()] = e.text
+
+    if add_environment_variables:
+        platform_specific.create_environment_variables(environment_variables)
+
+    return environment_variables
+
+
 if __name__ == "__main__":
     help(__name__)
+    get_project_xml_data()
