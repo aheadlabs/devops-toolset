7730cd684b0a57a0ddb3ed8fd44fb8331303c27c
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-29 13:43:00 +0200

    Added docstrings.

--- wordpress/tools.py
+++ wordpress/tools.py
@@ -6,23 +6,63 @@ import logging
 app: App = App()
 
 
-def get_constants() -> dict:
+def get_constants(path: str) -> dict:
+    """Gets all the constants from a WordPress constants file.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/wordpress-constants-schema.json
+
+    Args:
+        path: Full path to the WordPress constants file.
+    """
     pass
 
 
-def get_project_structure() -> dict:
+def get_project_structure(path: str) -> dict:
+    """Gets the project structure from a WordPress project structure file.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/project-structure-schema.json
+
+    Args:
+        path: Full path to the WordPress project structure file.
+    """
     pass
 
 
-def get_site_environments() -> dict:
+def get_site_environments(path: str) -> dict:
+    """Gets the project structure from a WordPress project structure file.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/wordpress-site-environments-schema.json
+
+    Args:
+        path: Full path to the WordPress project structure file.
+    """
     pass
 
 
-def get_site_configuration() -> dict:
+def get_site_configuration(path: str) -> dict:
+    """Gets the WordPress site configuration from a site configuration file.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/wordpress-site-schema.json
+
+    Args:
+        path: Full path to the WordPress project structure file.
+    """
     pass
 
 
-def create_project_structure() -> bool:
+def create_project_structure(project_structure: dict) -> bool:
+    """Creates the project structure from a site configuration file.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/wordpress-site-schema.json
+
+    Args:
+        project_structure: Parsed WordPress project structure file.
+    """
     pass
 
 
