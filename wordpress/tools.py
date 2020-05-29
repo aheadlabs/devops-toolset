c004aa8fae1a5bdbcfbf81b1b756be10753d5824
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-05-29 23:14:33 +0200

    Added docstrings.

--- wordpress/tools.py
+++ wordpress/tools.py
@@ -31,7 +31,7 @@ def get_project_structure(path: str) -> dict:
 
 
 def get_site_environments(path: str) -> dict:
-    """Gets the project structure from a WordPress project structure file.
+    """Gets the project structure from a WordPress site environment file.
 
     For more information see:
         http://dev.aheadlabs.com/schemas/json/wordpress-site-environments-schema.json
@@ -50,6 +50,20 @@ def get_site_configuration(path: str) -> dict:
 
     Args:
         path: Full path to the WordPress project structure file.
+    """
+    pass
+
+
+def get_site_configuration_path_from_environment(environment_path: str, environment_name: str) -> str:
+    """Gets the path to the WordPress site configuration from a environment.
+
+    For more information see:
+        http://dev.aheadlabs.com/schemas/json/wordpress-site-schema.json
+        http://dev.aheadlabs.com/schemas/json/wordpress-site-environments-schema.json
+
+    Args:
+        environment_path: Full path to the WordPress site environment file.
+        environment_name: Name of the environment to be got.
     """
     pass
 
--- wordpress/wp_cli.py
+++ wordpress/wp_cli.py
@@ -2,51 +2,189 @@
 
 from core.app import App
 import logging
+from enum import Enum
 
 app: App = App()
 
 
-def install_wp_cli() -> bool:
+class ValueType(Enum):
+    """Defines value types for values at the wp-config.php file"""
+
+    CONSTANT = 1,
+    VARIABLE = 2
+
+
+def install_wp_cli(install_path: str = "/usr/local/bin/wp"):
+    """Downloads and installs the latest version of WP-CLI.
+
+    For more information see:
+        https://make.wordpress.org/cli/handbook/installing/
+
+    Args:
+        install_path: Path where WP-CLI will be installed. It must be in the
+            PATH/BIN of the operating system.
+    """
     pass
 
 
-def download_wordpress() -> bool:
+def download_wordpress(site_configuration: dict, destination_path: str):
+    """Downloads the latest version of the WordPress core files using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/core/download/
+
+    Args:
+        site_configuration: parsed site configuration.
+        destination_path: Path where WP-CLI will be downloaded.
+    """
     pass
 
 
-def install_wordpress() -> bool:
+def install_wordpress(site_configuration: dict, wordpress_path: str, wordpress_admin_password: str):
+    """Installs WordPress core files using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/core/install/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+        wordpress_admin_password: Password for the WordPress administrator user
+    """
     pass
 
 
-def reset_database() -> bool:
+def reset_database(site_configuration: dict, wordpress_path: str):
+    """Removes all WordPress core tables from the database using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/db/reset/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+    """
     pass
 
 
-def import_database() -> bool:
+def import_database(site_configuration: dict, wordpress_path: str, dump_file_path: str):
+    """Imports a WordPress database from a dump file using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/db/import/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+        dump_file_path: Path to dump file to be imported.
+    """
     pass
 
 
-def set_database_configuration() -> bool:
+def set_database_configuration(site_configuration: dict, wordpress_path: str, database_user_password: str):
+    """Sets all WordPress database configuration parameters in wp-config.php
+    using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/config/set/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+        database_user_password: Password for the database user configured in at
+            the wp-config.php file.
+    """
     pass
 
 
-def export_database() -> bool:
+def export_database(site_configuration: dict, wordpress_path: str, dump_file_path: str):
+    """Exports a WordPress database to a dump file using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/db/export/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+        dump_file_path: Path to the destination dump file.
+    """
     pass
 
 
-def create_configuration_file() -> bool:
+def create_configuration_file(site_configuration: dict, wordpress_path: str, database_user_password: str):
+    """Creates the wp-config-php WordPress configuration file using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/config/create/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+        database_user_password: Password for the database user configured in at
+            the wp-config.php file.
+    """
     pass
 
 
-def set_configuration_constant() -> bool:
+def set_configuration_value(name: str, value: str, value_type: ValueType, wordpress_path: str):
+    """Creates or updates a value (constant or variable) at the wp-config-php
+    WordPress configuration file using WP-CLI.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/config/set/
+
+    Args:
+        name: Name for the constant or variable.
+        value: Value for the constant or variable.
+        value_type: Type to be created (constant or value).
+        wordpress_path: Path to WordPress files.
+    """
     pass
 
 
-def update_database_option() -> bool:
+def update_database_option(option_name: str, option_value: str, wordpress_path: str):
+    """Updates an option at the wp_options (*) table in the WordPress
+    database using WP-CLI.
+
+    (*) wp_ prefix could change, but this function will work anyway because
+        its value is obtained from the wp-config.php configuration file.
+    For more information see:
+        https://developer.wordpress.org/cli/commands/option/update/
+
+    Args:
+        option_name: Name for the option.
+        option_value: Value for the option.
+        wordpress_path: Path to WordPress files.
+    """
     pass
 
 
-def install_theme() -> bool:
+def install_theme_from_configuration_file(site_configuration: dict, wordpress_path: str):
+    """Creates the wp-config-php WordPress configuration file using WP-CLI.
+
+    All parameters are obtained from a site configuration file.
+
+    For more information see:
+        https://developer.wordpress.org/cli/commands/theme/install/
+
+    Args:
+        site_configuration: parsed site configuration.
+        wordpress_path: Path to WordPress files.
+    """
     pass
 
 
