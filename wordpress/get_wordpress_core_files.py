423a5ae67eab1841bda5c25b976aa7b4d85bac78
Alberto Carbonell Rubio <cr.alberto2@gmail.com>
2020-06-08 18:59:50 +0200

    Moved argument parsing to __main__ location in order to avoid test failure at launching this script

--- wordpress/get_wordpress_core_files.py
+++ wordpress/get_wordpress_core_files.py
@@ -3,32 +3,16 @@
 import argparse
 import wordpress.wptools
 import wordpress.wp_cli
-import filesystem.paths
 from core.app import App
 from core.LiteralsCore import LiteralsCore
 from wordpress.Literals import Literals as WordpressLiterals
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--wordpress-path")
-parser.add_argument("--environment-path")
-parser.add_argument("--environment-name")
-args, args_unknown = parser.parse_known_args()
 
 app: App = App()
 literals = LiteralsCore([WordpressLiterals])
 
 
-def main(wordpress_path: str = None, environment_path: str = None, environment_name: str = None):
+def main(wordpress_path, environment_path, environment_name):
     """Downloads the last version of WordPress core files"""
-
-    if not filesystem.paths.is_valid_path(wordpress_path):
-        raise ValueError(literals.get("wp_wordpress_path_mandatory"))
-
-    if not filesystem.paths.is_valid_path(environment_path):
-        raise ValueError(literals.get("wp_environment_path_mandatory"))
-
-    if not environment_name or environment_name.strip() == "":
-        raise ValueError(literals.get("wp_environment_name_mandatory"))
 
     site_configuration_path: str = \
         wordpress.wptools.get_site_configuration_path_from_environment(environment_path, environment_name)
@@ -39,4 +23,9 @@ def main(wordpress_path: str = None, environment_path: str = None, environment_n
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--wordpress-path", required=True)
+    parser.add_argument("--environment-path", required=True)
+    parser.add_argument("--environment-name", required=True)
+    args, args_unknown = parser.parse_known_args()
     main(args.wordpress_path, args.environment_path, args.environment_name)
