c25cdeaf1a66e192cb26b1bab5deb0ba9f07b23a
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-06-15 14:31:04 +0200

    Refactored literals order.

--- tools/Literals.py
+++ tools/Literals.py
@@ -12,13 +12,13 @@ class Literals(ValueDictsBase):
     # Add your core literal dictionaries here
     _info = {
         "git_purging_gitkeep": _("Purging .gitkeep file at {path}"),
-        "val_path_argument_not_valid": _("The path specified in the {argument} argument is invalid or does not exist."),
         "git_repo_to_be_created": _("The repository is going to be created"),
         "git_repo_created": _("The repository has been created"),
+        "val_path_argument_not_valid": _("The path specified in the {argument} argument is invalid or does not exist."),
     }
 
     _errors = {
-        "git_regex1cg": _("RegEx must have 1 capture group. No less, no more."),
-        "git_non_valid_dir_path": _("Path must be an existent dir."),
         "git_err_create_repo": _("Git error: repository couldn't be created"),
+        "git_non_valid_dir_path": _("Path must be an existent dir."),
+        "git_regex1cg": _("RegEx must have 1 capture group. No less, no more."),
     }
