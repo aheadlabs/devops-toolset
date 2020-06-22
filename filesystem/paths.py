9d79325f38037474c457557d9a9fb93d5ae49d85
Iván Sainz <ivan.sainz@aheadlabs.com>
2020-06-22 02:33:05 +0200

    Added log.

--- filesystem/paths.py
+++ filesystem/paths.py
@@ -208,7 +208,7 @@ def get_project_xml_data(add_environment_variables: bool = True, project_xml_pat
         project_xml_path = pathlib.Path.joinpath(app.settings.root_path, "project.xml")
     else:
         project_xml_path = pathlib.Path(project_xml_path)
-    logging.info(literals.get("fs_project_path_is").format(project_xml_path))
+    logging.info(literals.get("fs_project_path_is").format(path=project_xml_path))
     xml = ElementTree.parse(str(project_xml_path)).getroot()
 
     environment_variables = {}
