800e5a5ab1a0dfec28f5e98df62fa9e7d37b94ff
Alberto Carbonell Rubio <cr.alberto2@gmail.com>
2020-10-02 13:51:33 +0200

    Covered more banches in generate_wordpress script

--- project_types/wordpress/tests/generate_wordpress_test.py
+++ project_types/wordpress/tests/generate_wordpress_test.py
@@ -11,27 +11,35 @@ from project_types.wordpress import constants
 
 @patch("logging.critical")
 @patch("core.log_tools.log_indented_list")
-@patch("project_types.wordpress.wptools.install_plugins_from_configuration_file")
-@patch("project_types.wordpress.wptools.install_theme_from_configuration_file")
-@patch("project_types.wordpress.wptools.install_wordpress_site")
-@patch("project_types.wordpress.wptools.set_wordpress_config_from_configuration_file")
-@patch("project_types.wordpress.wptools.download_wordpress")
-@patch("project_types.wordpress.generate_wordpress.setup_devops_toolset")
-@patch("project_types.wordpress.wptools.start_basic_project_structure")
-@patch("project_types.wordpress.wptools.get_wordpress_path_from_root_path")
-@patch("project_types.wordpress.wptools.get_site_configuration_from_environment")
-@patch("project_types.wordpress.wptools.get_required_file_paths")
 @patch("filesystem.paths.files_exist_filtered")
 def test_main_given_required_files_when_not_present_and_not_localhost_then_raise_error(
-        files_exist_mock, get_required_files_mock, get_site_config_mock, get_wordpress_path, start_basic_structure_mock,
-        setup_devops_toolset_mock, download_wordpress_mock, set_wordpress_config_mock, install_wordpress_site_mock,
-        install_theme_mock, install_plugins_mock, log_indented_mock, wordpressdata):
+        files_exist_mock, log_indented_mock, logging_mock, wordpressdata):
     """ Given required files filter, when not present in root_path and not localhost environment, then raise error"""
     # Arrange
     required_files = ["file1", "file2", "file3"]
     files_exist_mock.return_value = required_files
     environment = "not_localhost"
     root_path = wordpressdata.root_path
+    # Act
+    with pytest.raises(ValueError) as value_error:
+        sut.main(root_path, "root", "root", environment)
+    # Assert
+    assert str(value_error.value) == sut.literals.get("wp_required_files_not_found").format(path=root_path)
+
+
+@patch("clint.textui.prompt.yn")
+@patch("logging.critical")
+@patch("core.log_tools.log_indented_list")
+@patch("filesystem.paths.files_exist_filtered")
+def test_main_given_required_files_when_not_present_and_localhost_and_no_defaults_then_raise_error(
+        files_exist_mock, log_indented_mock, logging_mock, prompt_mock, wordpressdata):
+    """ Given required files filter, when not present in root_path and not localhost environment, then raise error"""
+    # Arrange
+    required_files = ["file1", "file2", "file3"]
+    files_exist_mock.return_value = required_files
+    environment = "localhost"
+    root_path = wordpressdata.root_path
+    prompt_mock.return_value = False
     # Act
     with pytest.raises(ValueError) as value_error:
         sut.main(root_path, "root", "root", environment)
