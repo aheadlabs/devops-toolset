025ab449dfdd528041a0cf75c8a82fc7700b0f4a
Alberto Carbonell Rubio <cr.alberto2@gmail.com>
2021-02-11 13:20:04 +0100

    Added linux scaffold of commands and literals

---
+++ project_types/linux/Literals.py
@@ -0,0 +1,15 @@
+"""linux module literals"""
+
+from core.app import App
+from core.ValueDictsBase import ValueDictsBase
+
+app: App = App()
+
+
+class Literals(ValueDictsBase):
+    """ValueDicts for the linux module."""
+
+    _info = {
+    }
+    _errors = {
+    }
---
+++ project_types/linux/commands.py
@@ -0,0 +1,14 @@
+"""linux module commands"""
+
+from core.ValueDictsBase import ValueDictsBase
+from core.app import App
+
+app: App = App()
+
+
+class Commands(ValueDictsBase):
+    """Commands for the linux module."""
+
+    # Add your linux literal dictionaries here
+    _commands = {
+    }
