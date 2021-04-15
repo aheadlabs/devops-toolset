31af1ba8bce2c2ef1c3293e25e70f25606056f76
Alberto Carbonell Rubio <alberto.carbonell@aheadlabs.com>
2021-04-15 11:32:03 +0200

    Improved setup.py to get requirements from requirements.txt

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 """Project setup"""
 
 import setuptools
+import os
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+with open("requirements.txt", "r", encoding="utf-8") as req_file:
+    install_requires = req_file.read().splitlines()
 
 setuptools.setup(
     name='devops-toolset',
@@ -14,6 +18,7 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     packages=setuptools.find_packages(),
+    install_requires=install_requires,
     url='https://github.com/aheadlabs/devops-toolset/',
     license='https://github.com/aheadlabs/devops-toolset/blob/master/LICENSE',
     author='Ivan Sainz | Alberto Carbonell',
