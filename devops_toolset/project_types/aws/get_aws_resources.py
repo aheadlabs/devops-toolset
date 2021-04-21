f358c4ad35b4a947799f3ae76f9405faee0ac270
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-04-21 16:14:16 +0200

    Added the hosted zone parameter to avoid hardcoding.

--- devops_toolset/project_types/aws/get_aws_resources.py
+++ devops_toolset/project_types/aws/get_aws_resources.py
@@ -7,11 +7,13 @@ import tools.argument_validators
 import tools.cli as cli
 
 
-def main(json_path: str):
+def main(json_path: str, hosted_zone: str):
     """Gets the AWS resources and writes them to a JSON file.
 
     Args:
         json_path: Path to the JSON file to be written.
+        hosted_zone: DNS zone ID to be queried.
+            ie: /hostedzone/ABCDEFGHIJKL123456789 (must pass everything)
     """
 
     resources: dict = {}
@@ -38,8 +40,11 @@ def main(json_path: str):
         "aws s3api list-buckets --query \"Buckets[].Name\""))
     resources["route53_hosted_zones"] = json.loads(cli.call_subprocess_with_result(
         "aws route53 list-hosted-zones --query \"HostedZones[].Name\""))
-    resources["route53_recordsets"] = json.loads(cli.call_subprocess_with_result(
-        "aws route53 list-resource-record-sets --hosted-zone-id /hostedzone/Z07884893HQ7BXAS8FCQT"))
+
+    if hosted_zone is not None:
+        resources["route53_recordsets"] = json.loads(cli.call_subprocess_with_result(
+            f"aws route53 list-resource-record-sets --hosted-zone-id {hosted_zone}"))
+
     resources["iam_custom_policies"] = json.loads(cli.call_subprocess_with_result(
         "aws iam list-policies --scope Local --query \"Policies[].PolicyName\""))
     resources["iam_roles"] = json.loads(cli.call_subprocess_with_result(
@@ -52,6 +57,7 @@ def main(json_path: str):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("json-path", action=tools.argument_validators.PathValidator)
+    parser.add_argument("--hosted-zone", default=None)
     args, args_unknown = parser.parse_known_args()
 
-    main(args.json_path)
+    main(args.json_path, args.hosted_zone)
