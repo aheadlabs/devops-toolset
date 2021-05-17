98fd8049b1402d537931c70f401099479ed4edb6
Iván Sainz <ivan.sainz@aheadlabs.com>
2021-05-17 13:06:15 +0200

    Added dotnet utils for parsing the project version in a .csproj file.

--- devops_toolset/project_types/dotnet/Literals.py
+++ devops_toolset/project_types/dotnet/Literals.py
@@ -11,10 +11,11 @@ class Literals(ValueDictsBase):
 
     _info = {
         "dotnet_restore_before": "Launching dotnet restore inside {path}. Please wait..",
-        "dotnet_build_before": "Launching dotnet build inside {path}. Please wait.."
+        "dotnet_build_before": "Launching dotnet build inside {path}. Please wait..",
+        "dotnet_project_version": "The project version is {version}",
     }
     _errors = {
         "dotnet_restore_err": "Something went wrong while restoring {path}. Please check the logs and try again.",
-        "dotnet_build_err": "Something went wrong while building {path}. Please check the logs and try again."
+        "dotnet_build_err": "Something went wrong while building {path}. Please check the logs and try again.",
     }
 
