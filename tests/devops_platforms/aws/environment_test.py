ab20640e4a8e72e3030de935ca88f2b1137841ba
Alberto Carbonell Rubio <alberto.carbonell@aheadlabs.com>
2021-10-12 18:06:33 +0200

    Adapted environment variables test.

--- tests/devops_platforms/aws/environment_test.py
+++ tests/devops_platforms/aws/environment_test.py
@@ -1,4 +1,6 @@
 """Unit core for the environment file"""
+import os
+
 import pytest
 
 from devops_toolset.core.CommandsCore import CommandsCore
@@ -13,25 +15,21 @@ linux_commands = CommandsCore([LinuxCommands])
 
 
 @patch("logging.info")
-@patch("devops_toolset.tools.cli.call_subprocess")
 def test_create_environment_variables_given_dict_when_not_empty_calls_to_create_env_variables_command(
-        call_subprocess_mock, logger_mock, platformdata):
-    """Given a dictionary, when it is not empty, writes environment variables
-    to stdout in the Azure DevOps notation"""
+        logger_mock, platformdata):
+    """Given a dictionary, when it is not empty, creates environment variables"""
 
     # Arrange
     environment_variables = platformdata.environment_variables_dict
-    expected_command_1 = linux_commands.get("create_env_variable").\
-        format(variable_name="env_var_1", variable_value="value1")
-    expected_command_2 = linux_commands.get("create_env_variable").\
-        format(variable_name="env_var_2", variable_value="value2")
 
     # Act
     sut.create_environment_variables(environment_variables)
-    calls = [call(expected_command_1), call(expected_command_2)]
 
     # Assert
-    call_subprocess_mock.assert_has_calls(calls)
+    for key in environment_variables.keys():
+        assert key in os.environ
+        del os.environ[key]  # Cleanup: We need to remove the example values from the env variables.
+
 
 # endregion
 
