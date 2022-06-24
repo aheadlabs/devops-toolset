0941ab27d025e753700f1dc7fef0ad7fc8b56586
Iván Sainz <ivan.sainz@aheadlabs.com>
2022-06-25 01:48:31 +0200

    Added check for Entity Framework migrations suitableness for a given Git branch.

--- tests/project_types/dotnet/ci_pipeline_kickoff_test.py
+++ tests/project_types/dotnet/ci_pipeline_kickoff_test.py
@@ -25,23 +25,24 @@ def test_main_calls_devops_toolset_configure_main(configure_main_mock):
     language = "en"
 
     # Act
-    sut.main(devops_platform, None, None, True)
+    sut.main(devops_platform, None, None, None, True)
 
     # Assert
-    configure_main_mock.assert_called_with(devops_platform, language)
+    configure_main_mock.assert_called_with(devops_platform=devops_platform, language=language)
 
 
 @patch("devops_toolset.project_types.dotnet.utils.get_csproj_project_version")
 def test_main_calls_get_csproj_project_version(get_csproj_project_version_mock):
     """ Given skip_get_public_ip_address argument, should call get_public_ip_method when false method """
+
     # Arrange
     csproj_path = "pathto/csproj"
 
     # Act
-    sut.main(None, csproj_path, None, True)
+    sut.main(None, csproj_path, None, None, True)
 
     # Assert
-    get_csproj_project_version_mock.assert_called_with(csproj_path)
+    get_csproj_project_version_mock.assert_called_with(csproj_path=csproj_path)
 
 
 @patch("devops_toolset.tools.git.get_current_branch_simplified")
@@ -51,10 +52,30 @@ def test_main_calls_get_current_branch_simplified(get_current_branch_mock):
     current_branch = "refs/heads/feature/myfeature"
 
     # Act
-    sut.main(None, None, current_branch, True)
+    sut.main(None, None, current_branch, None, True)
 
     # Assert
-    get_current_branch_mock.assert_called_with(current_branch)
+    get_current_branch_mock.assert_called_with(branch=current_branch)
+
+
+@patch("devops_toolset.project_types.dotnet.entity_framework.check_branch_suitableness_for_migrations")
+@patch("devops_toolset.tools.git.get_current_branch_simplified")
+def test_main_calls_check_branch_suitableness_for_migrations(get_current_branch_mock, check_branch_mock):
+    """ Given skip_get_public_ip_address argument, should call get_public_ip_method when false method """
+    # Arrange
+    current_branch = "refs/heads/feature/myfeature"
+    current_branch_simplified = "feature/myfeature"
+    get_current_branch_mock.return_value = current_branch_simplified
+    migrations_suitable_branches = ["dev", "main"]
+
+    # Act
+    sut.main(None, None, current_branch, migrations_suitable_branches, True)
+
+    # Assert
+    check_branch_mock.assert_called_with(
+        current_simplified_branch=current_branch_simplified,
+        suitable_branches=migrations_suitable_branches
+    )
 
 
 @patch("devops_toolset.tools.http_protocol.get_public_ip_address")
@@ -64,7 +85,7 @@ def test_main_calls_get_public_ip_address(get_public_ip_address_mock):
     # Arrange
 
     # Act
-    sut.main(None, None, None, False)
+    sut.main(None, None, None, None, False)
 
     # Assert
     get_public_ip_address_mock.assert_called()
--- tests/project_types/dotnet/entity_framework_test.py
+++ tests/project_types/dotnet/entity_framework_test.py
@@ -1,4 +1,5 @@
 """ Unit tests for the dotnet/entity_framework.py module"""
+import pytest
 
 from devops_toolset.core.app import App
 from devops_toolset.core.CommandsCore import CommandsCore
@@ -17,104 +18,6 @@ import pathlib
 app: App = App()
 literals = LiteralsCore([DotnetLiterals])
 commands = CommandsCore([DotnetCommands])
-
-
-# region drop_database()
-
-@patch("devops_toolset.tools.cli.call_subprocess_with_result")
-@patch("logging.info")
-def test_drop_database_calls_subprocess(logging_info_mock, subprocess_mock):
-    """ Calls subprocess for executing EF command. """
-
-    # Arrange
-    startup_project_path: str = "path/to/project"
-    environment: str = "Staging"
-
-    # Act
-    sut.drop_database(startup_project_path, environment)
-
-    # Assert
-    subprocess_mock.assert_called()
-
-# endregion drop_database()
-
-# region generate_migration_sql_script()
-
-
-@mock.patch.object(sut, "__get_migrations_list")
-@mock.patch.object(sut, "__get_first_migration_not_applied")
-@mock.patch.object(sut, "__generate_sql_script")
-def test_generate_migration_sql_script_calls_generate_sql_script(
-        generate_sql_script_mock, get_first_migration_not_applied_mock, get_migrations_list_mock, migrationsdata):
-    """ Calls __generate_sql_script with required data"""
-    # Arrange
-    startup_project_path: str = ""
-    environment: str = ""
-    script_path: str = "#date#"
-    migration_date: str = "20220529212512"
-    migration_name: str = "Second-V2"
-    get_migrations_list_mock.return_value = json.loads(migrationsdata.one_migration_and_applied)
-    get_first_migration_not_applied_mock.return_value = migration_name, migration_date
-    migrations, applied_migrations, last_migration_applied = \
-        sut.__parse_data_from_migrations_json_array(json.loads(migrationsdata.one_migration_and_applied))
-    # Act
-    sut.generate_migration_sql_script(startup_project_path, environment, script_path)
-
-    # Assert
-    generate_sql_script_mock.assert_called_once_with(
-        startup_project_path, script_path.replace("#date#", migration_date), environment, last_migration_applied)
-
-
-# endregion generate_migration_sql_script()
-
-# region generate_migration_sql_scripts_for_all_environments()
-
-@patch("logging.info")
-@mock.patch.object(sut, "generate_migration_sql_script")
-@mock.patch.object(utils, "get_appsettings_environments")
-def test_generate_migration_sql_scripts_for_all_environments_calls_generate_migration_sql_script(
-        get_appsettings_environments_mock, generate_migration_sql_script_mock, logging_info_mock):
-    """ Calls generate_migration_sql_script with required data """
-    # Arrange
-    environments = ["staging", "production"]
-    get_appsettings_environments_mock.return_value = environments
-    startup_project_path = ""
-    scripts_base_path = "my_path/scripts"
-    expected_script_path_0 = pathlib.Path.joinpath(
-        pathlib.Path(scripts_base_path), f"database-migration-staging-from-#date#.sql")
-    expected_script_path_1 = pathlib.Path.joinpath(
-        pathlib.Path(scripts_base_path), f"database-migration-production-from-#date#.sql")
-    expected_calls = [call(startup_project_path, environments[0], str(expected_script_path_0)),
-                      call(startup_project_path, environments[1], str(expected_script_path_1))]
-
-    # Act
-    sut.generate_migration_sql_scripts_for_all_environments(startup_project_path, scripts_base_path, False)
-
-    # Assert
-    generate_migration_sql_script_mock.assert_has_calls(expected_calls)
-
-
-# endregion generate_migration_sql_scripts_for_all_environments()
-
-# region reset_database()
-
-
-@patch("devops_toolset.tools.cli.call_subprocess_with_result")
-@patch("logging.info")
-def test_drop_database_calls_subprocess(logging_info_mock, subprocess_mock):
-    """ Calls subprocess for executing EF command. """
-
-    # Arrange
-    startup_project_path: str = "path/to/project"
-    environment: str = "Staging"
-
-    # Act
-    sut.reset_database(startup_project_path, environment)
-
-    # Assert
-    subprocess_mock.assert_called()
-
-# endregion drop_database()
 
 # region __get_first_migration_not_applied()
 
@@ -215,3 +118,124 @@ def test_parse_data_from_migrations_json_array_given_migrations_list_returns_par
     assert last_migration_applied == migrations_list[0]["id"]
 
 # endregion __parse_data_from_migrations_json_array()
+
+# region check_branch_suitableness_for_migrations()
+
+
+@pytest.mark.parametrize("current_branch_simplified, suitable_branches_list, expected", [
+    ("dev", ["dev", "main"], True), ("feature/unit-tests", ["dev", "main"], False)
+])
+def test_check_branch_suitableness_for_migrations_writes_environment_variable(
+        current_branch_simplified, suitable_branches_list, expected):
+    """Given a Git branch name, writes an environment variable with the boolean
+    result."""
+
+    # Arrange
+    environment_variable_name = "MY_ENV_VAR"
+
+    # Act
+    result = sut.check_branch_suitableness_for_migrations(
+        current_branch_simplified, suitable_branches_list, environment_variable_name)
+
+    # Assert
+    assert result is expected
+
+# endregion check_branch_suitableness_for_migrations()
+
+# region drop_database()
+
+
+@patch("devops_toolset.tools.cli.call_subprocess_with_result")
+@patch("logging.info")
+def test_drop_database_calls_subprocess(logging_info_mock, subprocess_mock):
+    """ Calls subprocess for executing EF command. """
+
+    # Arrange
+    startup_project_path: str = "path/to/project"
+    environment: str = "Staging"
+
+    # Act
+    sut.drop_database(startup_project_path, environment)
+
+    # Assert
+    subprocess_mock.assert_called()
+
+# endregion drop_database()
+
+# region generate_migration_sql_script()
+
+
+@mock.patch.object(sut, "__get_migrations_list")
+@mock.patch.object(sut, "__get_first_migration_not_applied")
+@mock.patch.object(sut, "__generate_sql_script")
+def test_generate_migration_sql_script_calls_generate_sql_script(
+        generate_sql_script_mock, get_first_migration_not_applied_mock, get_migrations_list_mock, migrationsdata):
+    """ Calls __generate_sql_script with required data"""
+    # Arrange
+    startup_project_path: str = ""
+    environment: str = ""
+    script_path: str = "#date#"
+    migration_date: str = "20220529212512"
+    migration_name: str = "Second-V2"
+    get_migrations_list_mock.return_value = json.loads(migrationsdata.one_migration_and_applied)
+    get_first_migration_not_applied_mock.return_value = migration_name, migration_date
+    migrations, applied_migrations, last_migration_applied = \
+        sut.__parse_data_from_migrations_json_array(json.loads(migrationsdata.one_migration_and_applied))
+    # Act
+    sut.generate_migration_sql_script(startup_project_path, environment, script_path)
+
+    # Assert
+    generate_sql_script_mock.assert_called_once_with(
+        startup_project_path, script_path.replace("#date#", migration_date), environment, last_migration_applied)
+
+
+# endregion generate_migration_sql_script()
+
+# region generate_migration_sql_scripts_for_all_environments()
+
+@patch("logging.info")
+@mock.patch.object(sut, "generate_migration_sql_script")
+@mock.patch.object(utils, "get_appsettings_environments")
+def test_generate_migration_sql_scripts_for_all_environments_calls_generate_migration_sql_script(
+        get_appsettings_environments_mock, generate_migration_sql_script_mock, logging_info_mock):
+    """ Calls generate_migration_sql_script with required data """
+    # Arrange
+    environments = ["staging", "production"]
+    get_appsettings_environments_mock.return_value = environments
+    startup_project_path = ""
+    scripts_base_path = "my_path/scripts"
+    expected_script_path_0 = pathlib.Path.joinpath(
+        pathlib.Path(scripts_base_path), f"database-migration-staging-from-#date#.sql")
+    expected_script_path_1 = pathlib.Path.joinpath(
+        pathlib.Path(scripts_base_path), f"database-migration-production-from-#date#.sql")
+    expected_calls = [call(startup_project_path, environments[0], str(expected_script_path_0)),
+                      call(startup_project_path, environments[1], str(expected_script_path_1))]
+
+    # Act
+    sut.generate_migration_sql_scripts_for_all_environments(startup_project_path, scripts_base_path, False)
+
+    # Assert
+    generate_migration_sql_script_mock.assert_has_calls(expected_calls)
+
+
+# endregion generate_migration_sql_scripts_for_all_environments()
+
+# region reset_database()
+
+
+@patch("devops_toolset.tools.cli.call_subprocess_with_result")
+@patch("logging.info")
+def test_drop_database_calls_subprocess(logging_info_mock, subprocess_mock):
+    """ Calls subprocess for executing EF command. """
+
+    # Arrange
+    startup_project_path: str = "path/to/project"
+    environment: str = "Staging"
+
+    # Act
+    sut.reset_database(startup_project_path, environment)
+
+    # Assert
+    subprocess_mock.assert_called()
+
+# endregion drop_database()
