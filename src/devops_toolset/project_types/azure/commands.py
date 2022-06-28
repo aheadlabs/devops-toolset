db9fdefa9fb9b30eda28b0a3af1d55524bc372fb
Iván Sainz <ivan.sainz@aheadlabs.com>
2022-06-28 22:58:57 +0200

    Added Azure CLI native log parameter to all commands and functions.

--- src/devops_toolset/project_types/azure/commands.py
+++ src/devops_toolset/project_types/azure/commands.py
@@ -2,8 +2,16 @@
 
 from devops_toolset.core.ValueDictsBase import ValueDictsBase
 from devops_toolset.core.app import App
+from enum import Enum
 
 app: App = App()
+
+
+class Log(Enum):
+    """Log level for Azure CLI commands"""
+    OFF = 0
+    VERBOSE = 1
+    DEBUG = 2
 
 
 class Commands(ValueDictsBase):
@@ -13,13 +21,13 @@ class Commands(ValueDictsBase):
     _commands = {
         "azure_cli_db_mysql_flexible_server_execute":
             "az mysql flexible-server execute -n {server_name} -u {admin_user} -p {admin_password} "
-            "-d {database_name} {file_path} {query}",
+            "-d {database_name} {file_path} {query} {log}",
         "azure_cli_db_mysql_flexible_server_firewall_rule_create":
             "az mysql flexible-server firewall-rule create -n {server_name} -g {resource_group} "
-            "-r {rule_name} --start-ip-address {start_ip_address} --end-ip-address {end_ip_address}",
+            "-r {rule_name} --start-ip-address {start_ip_address} --end-ip-address {end_ip_address} {log}",
         "azure_cli_db_mysql_flexible_server_firewall_rule_delete":
             "az mysql flexible-server firewall-rule delete -n {server_name} -g {resource_group} "
-            "-r {rule_name} -y",
+            "-r {rule_name} -y {log}",
         "azure_cli_extension_list": "az extension list",
         "azure_cli_extension_add": "az extension add --name {name}",
     }
--- src/devops_toolset/project_types/azure/database.py
+++ src/devops_toolset/project_types/azure/database.py
@@ -1,9 +1,9 @@
 """Provides tools for managing the Azure Database service"""
-
+import devops_toolset.project_types.azure.commands
 from devops_toolset.core.app import App
 from devops_toolset.core.CommandsCore import CommandsCore
 from devops_toolset.core.LiteralsCore import LiteralsCore
-from devops_toolset.project_types.azure.commands import Commands as AzureCommands
+from devops_toolset.project_types.azure.commands import Commands as AzureCommands, Log as Log
 from devops_toolset.project_types.azure.Literals import Literals as AzureLiterals
 from devops_toolset.tools import cli
 
@@ -18,12 +18,9 @@ commands = CommandsCore([AzureCommands])
 rule_name_fix_regex = "[^A-Za-z0-9-_]"
 
 
-def add_mysql_flexible_server_firewall_rule(
-        server_name: str,
-        resource_group: str,
-        rule_name: str,
-        ip_address: str,
-        end_ip_address: [str, None] = None) -> dict:
+def add_mysql_flexible_server_firewall_rule(server_name: str, resource_group: str, rule_name: str,
+                                            ip_address: str, end_ip_address: [str, None] = None,
+                                            log: Log = Log.OFF) -> dict:
     """Adds a firewall rule to MySQL Flexible server.
 
     Args:
@@ -35,6 +32,7 @@ def add_mysql_flexible_server_firewall_rule(
             address of the range.
         end_ip_address: End IP address of the range. If we are adding a unique
             IP address this value must be None.
+        log: Log level from OFF, VERBOSE or DEBUG.
 
     Returns:
         Operation result
@@ -45,7 +43,8 @@ def add_mysql_flexible_server_firewall_rule(
         resource_group=resource_group,
         rule_name=re.sub(rule_name_fix_regex, "-", rule_name),
         start_ip_address=ip_address,
-        end_ip_address=ip_address if end_ip_address is None else end_ip_address
+        end_ip_address=ip_address if end_ip_address is None else end_ip_address,
+        log="" if log == Log.OFF else f"--{log.name.lower()}"
     )
     logging.info(literals.get("azure_cli_executing_command").format(command=az_command))
 
@@ -55,9 +54,9 @@ def add_mysql_flexible_server_firewall_rule(
     return json.loads(result)
 
 
-def execute_mysql_flexible_server_sql_script(admin_user: str, admin_password: str,
-                                             server_name: str, database_name: str,
-                                             file_path: [str, None] = None, query: [str, None] = None) -> [str, None]:
+def execute_mysql_flexible_server_sql_script(admin_user: str, admin_password: str, server_name: str, database_name: str,
+                                             file_path: [str, None] = None, query: [str, None] = None,
+                                             log: Log = Log.OFF) -> [str, None]:
     """Executes a script (file) / query (text) against MySQL Flexible server.
 
     Args:
@@ -68,6 +67,7 @@ def execute_mysql_flexible_server_sql_script(admin_user: str, admin_password: st
         file_path: Path to the SQL file to be executed.
         query: Query text to be executed. If file_path is not None, query will
             be ignored.
+        log: Log level from OFF, VERBOSE or DEBUG.
     """
 
     # Check that parameters are correct
@@ -88,7 +88,8 @@ def execute_mysql_flexible_server_sql_script(admin_user: str, admin_password: st
         admin_password=admin_password,
         database_name=database_name,
         file_path=f"-f {file_path}" if file_path is not None else "",
-        query=f"-q \"{query}\"" if query is not None and file_path is None else ""
+        query=f"-q \"{query}\"" if query is not None and file_path is None else "",
+        log="" if log == Log.OFF else f"--{log.name.lower()}"
     )
     logging.info(literals.get("azure_cli_executing_command").format(command=az_command))
 
@@ -96,16 +97,15 @@ def execute_mysql_flexible_server_sql_script(admin_user: str, admin_password: st
     logging.info(literals.get("azure_cli_command_output").format(output=result))
 
 
-def remove_mysql_flexible_server_firewall_rule(
-        server_name: str,
-        resource_group: str,
-        rule_name: str) -> str:
+def remove_mysql_flexible_server_firewall_rule(server_name: str, resource_group: str, rule_name: str,
+                                               log: Log = Log.OFF) -> str:
     """Removes a firewall rule from MySQL Flexible server.
 
     Args:
         server_name: Name of the server we target.
         resource_group: Azure resource group that the server belongs to.
         rule_name: Name of the rule to be created.
+        log: Log level from OFF, VERBOSE or DEBUG.
 
     Returns:
         Operation result
@@ -114,7 +114,8 @@ def remove_mysql_flexible_server_firewall_rule(
     az_command: str = commands.get("azure_cli_db_mysql_flexible_server_firewall_rule_delete").format(
         server_name= server_name,
         resource_group=resource_group,
-        rule_name=re.sub(rule_name_fix_regex, "-", rule_name)
+        rule_name=re.sub(rule_name_fix_regex, "-", rule_name),
+        log="" if log == Log.OFF else f"--{log.name.lower()}"
     )
     logging.info(literals.get("azure_cli_executing_command").format(command=az_command))
 
