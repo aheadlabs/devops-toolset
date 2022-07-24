ad3026b0be5bc5195fe3b74fc3bdcf9558da5ba0
Iván Sainz <ivan.sainz@aheadlabs.com>
2022-07-24 23:28:25 +0200

    Changed function parameter name to avoid shadowing.

--- src/devops_toolset/project_types/dotnet/scaffold_webapi_solution.py
+++ src/devops_toolset/project_types/dotnet/scaffold_webapi_solution.py
@@ -275,16 +275,16 @@ def get_configuration(template_name: str):
     return f"./scaffolding_templates/{template_name}-template.json"
 
 
-def get_project_layers(template_config: dict, solution_name: str):
+def get_project_layers(template_configuration: dict, solution_name: str):
     """Returns a dict with all project folders.
 
     Args:
-        template_config: Template configuration.
+        template_configuration: Template configuration.
         solution_name: Name of the solution.
     """
 
     project_layers: dict = {}
-    for layer in template_config["layers"]:
+    for layer in template_configuration["layers"]:
         for project in layer["projects"]:
             project_layers[f"{solution_name}{project['name']}"] = layer["name"]
 
